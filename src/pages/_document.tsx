1e9029d3bc0287ce6c3af6a71a43ab556e510870
Hardik-Jain08 <hjain2734@gmail.com>
2023-11-09 02:09:16 +0530

    logo icon and title added with import fixes

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,5 +1,5 @@
-import { Html, Head, Main, NextScript } from 'next/document'
-import { responsiveSizesCSSVariables } from "../styles/responsiveSizes.styles";
+import { Html, Head, Main, NextScript } from "next/document";
+import { responsiveSizesCSSVariables } from "@/styles/responsiveSizes.styles";
 
 import { createGlobalStyle } from "styled-components";
 
@@ -8,14 +8,14 @@ const GlobalStyle = createGlobalStyle`
 `;
 
 export default function Document() {
-	return (
-		<Html lang='en'>
-			<Head />
-			<body>
-				<GlobalStyle />
-				<Main />
-				<NextScript />
-			</body>
-		</Html>
-	);
+  return (
+    <Html lang="en">
+      <Head />
+      <body>
+        <GlobalStyle />
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
 }
