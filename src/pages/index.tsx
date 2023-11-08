865a64d65fdf9dd1a713b6a4ba2150e7adad3936
Rohan Gupta <rohangupta1528@gmail.com>
2023-11-09 04:25:13 +0530

    Added Animations

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -72,7 +72,9 @@ export default function Home() {
 								</h1>
 							</HeadingGroup>
 							<Description>
-								<p>Your pocket plant expert</p>
+								<p className='font-light mt-2 text-gray-200'>
+									Your pocket plant expert
+								</p>
 								{/* <ButtonGroupCenteredUptoDesktop>
 									<SignUpButton
 										appearance='filled-white'
@@ -86,7 +88,7 @@ export default function Home() {
 								</ButtonGroupCenteredUptoDesktop> */}
 							</Description>
 						</HeaderTextContent>
-						<div className='grid md:grid-cols-3 justify-center'>
+						<div className='grid md:grid-cols-3 justify-center animate-fade'>
 							<div className='md:col-span-2'>
 								<PhoneIlloWrap>
 									<HomePageHeroIlloPhone />
@@ -118,161 +120,168 @@ export default function Home() {
 }
 
 const LooseDecorationsSetBottom = styled.div`
-  position: relative;
-  ${uptoDesktop} {
-    display: none;
-  }
+	position: relative;
+	${uptoDesktop} {
+		display: none;
+	}
 `;
 
 const DecoTopLeft = styled.div`
-  position: absolute;
-  left: 0;
-  top: 0;
+	position: absolute;
+	left: 0;
+	top: 0;
+	animation: moveUpAndDown 2s ease-out infinite alternate;
 `;
+
 const DecoTopRight = styled.div`
-  position: absolute;
-  right: 2em;
-  top: 0;
+	position: absolute;
+	right: 2em;
+	top: 0;
+	animation: moveUpAndDown 2s ease-in-out infinite alternate;
 `;
+
 const DecoMiddleLeft = styled.div`
-  position: absolute;
-  right: calc(100% + 0.5em);
-  bottom: 1em;
+	position: absolute;
+	right: calc(100% + 0.5em);
+	bottom: 1em;
+	animation: moveUpAndDown 3s ease-in infinite alternate;
 `;
+
 const DecoMiddleRight = styled.div`
-  position: absolute;
-  left: calc(100% - 30em);
-  bottom: 5em;
+	position: absolute;
+	left: calc(100% - 30em);
+	bottom: 5em;
+	animation: moveUpAndDown 4s ease-in-out infinite;
 `;
 
 const Header = styled.div`
-  position: relative;
+	position: relative;
 `;
 
 const TextAndIllustration = styled.div`
-  position: relative;
-  svg {
-    max-width: 100%;
-    height: auto;
-  }
+	position: relative;
+	svg {
+		max-width: 100%;
+		height: auto;
+	}
 `;
 
 const HeaderTextContent = styled.div`
-  ${onlyTablets} {
-    position: absolute;
-    top: 0;
-    left: 50%;
-    transform: translateX(-50%);
-  }
+	${onlyTablets} {
+		position: absolute;
+		top: 0;
+		left: 50%;
+		transform: translateX(-50%);
+	}
 `;
 
 const PhoneIlloWrap = styled.div`
-  display: none;
-  pointer-events: none;
-  ${onlyPhones} {
-    display: block;
-  }
+	display: none;
+	pointer-events: none;
+	${onlyPhones} {
+		display: block;
+	}
 `;
 
 const TabletIlloWrap = styled.div`
-  display: none;
-  pointer-events: none;
-  ${fromTablet} {
-    display: block;
-    padding-top: 6em;
-  }
-  ${fromTabletMd} {
-    padding-top: 4em;
-  }
-  ${fromDesktop} {
-    display: none;
-  }
+	display: none;
+	pointer-events: none;
+	${fromTablet} {
+		display: block;
+		padding-top: 6em;
+	}
+	${fromTabletMd} {
+		padding-top: 4em;
+	}
+	${fromDesktop} {
+		display: none;
+	}
 `;
 
 const DesktopIlloWrap = styled.div`
-  display: none;
-  pointer-events: none;
-  ${fromDesktop} {
-    display: block;
-    position: relative;
-  }
+	display: none;
+	pointer-events: none;
+	${fromDesktop} {
+		display: block;
+		position: relative;
+	}
 `;
 
 const HeadingGroup = styled.div`
-  ${onlyPhones} {
-    padding-bottom: 2em;
-  }
-  ${onlyTablets} {
-    padding-bottom: 1em;
-  }
-  ${fromDesktop} {
-    padding-bottom: 0;
-  }
-  h1.heading {
-    ${inter}
-    font-size: 5.8rem;
-    font-weight: 180;
-    ${fromTablet} {
-      font-size: 5.2rem;
-    }
-    ${fromTabletMd} {
-      font-size: 6.8rem;
-      font-weight: 163;
-    }
-    ${fromDesktop} {
-      font-size: 7.6rem;
-    }
-  }
-  h1,
-  p {
-    > span {
-      display: block;
-    }
-  }
+	${onlyPhones} {
+		padding-bottom: 2em;
+	}
+	${onlyTablets} {
+		padding-bottom: 1em;
+	}
+	${fromDesktop} {
+		padding-bottom: 0;
+	}
+	h1.heading {
+		${inter}
+		font-size: 5.8rem;
+		font-weight: 180;
+		${fromTablet} {
+			font-size: 5.2rem;
+		}
+		${fromTabletMd} {
+			font-size: 6.8rem;
+			font-weight: 163;
+		}
+		${fromDesktop} {
+			font-size: 7.6rem;
+		}
+	}
+	h1,
+	p {
+		> span {
+			display: block;
+		}
+	}
 `;
 
 const Description = styled.div`
-  text-align: center;
-  font-weight: 500;
-  padding-bottom: 3em;
-  margin-left: auto;
-  font-size: 1.6rem;
+	text-align: center;
+	font-weight: 500;
+	padding-bottom: 3em;
+	margin-left: auto;
+	font-size: 1.6rem;
 
-  > * + * {
-    margin-top: 1em;
-    margin-bottom: 1em;
-  }
+	> * + * {
+		margin-top: 1em;
+		margin-bottom: 1em;
+	}
 `;
 
 const LogoSetWrap = styled.div`
-  /* padding: 3em 0 3.5em; */
-  padding-top: 3em;
-  padding-bottom: 1em;
-  ${fromTablet} {
-    /* padding: 4em 0 4.5em; */
-    padding-top: 4em;
-    padding-bottom: 1.5em;
-  }
-  ${fromDesktopMl} {
-    /* padding: 5em 0 5.5em; */
-    padding-top: 5em;
-  }
+	/* padding: 3em 0 3.5em; */
+	padding-top: 3em;
+	padding-bottom: 1em;
+	${fromTablet} {
+		/* padding: 4em 0 4.5em; */
+		padding-top: 4em;
+		padding-bottom: 1.5em;
+	}
+	${fromDesktopMl} {
+		/* padding: 5em 0 5.5em; */
+		padding-top: 5em;
+	}
 `;
 
 const LooseDecorationsSetTop = styled.div`
-  position: relative;
-  margin-left: auto;
-  margin-right: auto;
-  max-width: 48rem;
-  ${fromTablet} {
-    display: none;
-  }
+	position: relative;
+	margin-left: auto;
+	margin-right: auto;
+	max-width: 48rem;
+	${fromTablet} {
+		display: none;
+	}
 `;
 const LooseDecorationsSetMiddle = styled.div`
-  position: relative;
-  ${uptoDesktop} {
-    display: none;
-  }
+	position: relative;
+	${uptoDesktop} {
+		display: none;
+	}
 `;
 // const LooseDecorationsSetBottom = styled.div`
 //   position: relative;
@@ -282,7 +291,7 @@ const LooseDecorationsSetMiddle = styled.div`
 // `;
 
 const SiteNavThemeControllerPointPositioner = styled.div`
-  position: absolute;
-  bottom: 0;
-  left: 0;
+	position: absolute;
+	bottom: 0;
+	left: 0;
 `;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,36 @@
 import type { Config } from 'tailwindcss'
 
 const config: Config = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
-  },
-  plugins: [],
-}
+	content: [
+		"./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+	],
+	theme: {
+		extend: {
+			backgroundImage: {
+				"gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+				"gradient-conic":
+					"conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+			},
+			animation: {
+				fade: "smoothAnimate 200ms ease-in-out",
+			},
+
+			// that is actual animation
+			keyframes: (theme: any) => ({
+				fadeIn: {
+					"0%": { opacity: 0 },
+					"100%": { opacity: 1 },
+				},
+				smoothAnimate: {
+					"0%": { transform: "translateY(0px)", opacity: 0 },
+					"40%": { transform: "translateY(-16px)", opacity: 0 },
+					"100%": { transform: "translateY(0px)", opacity: 1 },
+				},
+			}),
+		},
+	},
+	plugins: [],
+};
 export default config
