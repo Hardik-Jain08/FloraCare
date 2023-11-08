bd42d7f05584dd071111f964e65e30e594a1ba37
Hardik-Jain08 <hjain2734@gmail.com>
2023-11-09 01:20:43 +0530

    Seperated HeroSection and fixed imports

---
+++ src/components/HeroSection.tsx
@@ -0,0 +1,108 @@
+import { colors } from "@/styles/colors.styles";
+import { PropsWithChildren, ReactNode } from "react";
+import Spacing from "./Spacing";
+import { GridCanvas } from "./GridCanvas";
+import { cx } from "linaria";
+import styled from "styled-components";
+import { maxPageContentWidthStyleObject } from "@/styles/maxPageContentWidth.styles";
+
+const HeroSection = (
+	props: PropsWithChildren<{
+		largeHeading?: boolean;
+		backgroundColor?: string;
+		textColor?: string;
+		backdrop?: "grid" | "dots";
+		backdropColor?: string;
+		backdropPositionBottom?: string;
+		/** only available to the grid design */
+		backdropParallax?: boolean;
+		/** only available to the grid design */
+		backdropAnimateIn?: boolean;
+		backdropLayerChildren?: ReactNode;
+	}>
+) => {
+	const backdropColor = props.backdropColor ?? colors.purple100;
+	const hasBackground = props.backdrop || props.backdropColor;
+
+	return (
+		<HeroSectionWrap textColor={props.textColor}>
+			<Spacing size='xxl' />
+			{hasBackground && (
+				<HeroSectionBackdropLayer
+					id='HeroSectionBackdropLayer'
+					backgroundColor={props.backgroundColor}
+					offsetBottom={props.backdropPositionBottom}
+				>
+					<GridCanvas
+						color={backdropColor}
+						addBorderBottom
+						parallax={props.backdropParallax}
+						animateIn={props.backdropAnimateIn}
+					/>
+					{props.backdropLayerChildren}
+				</HeroSectionBackdropLayer>
+			)}
+			<Content className={cx(props.largeHeading && "largeHeading")}>
+				{props.children}
+			</Content>
+			{hasBackground && <Spacing size='sectionPadding' />}
+		</HeroSectionWrap>
+	);
+};
+
+export default HeroSection;
+
+const HeroSectionWrap = styled.header<{
+	textColor?: string;
+}>`
+	width: 100%;
+	position: relative;
+	text-align: center;
+	color: ${(p) => p.textColor ?? "inherit"};
+`;
+
+export const HeroSectionBackdropLayer = styled.div<{
+	backgroundColor?: string;
+	offsetBottom?: string;
+}>`
+	position: absolute;
+	top: 0;
+	right: 0;
+	background-color: ${(p) => p.backgroundColor ?? "transparent"};
+	bottom: ${(p) => p.offsetBottom || "0"};
+	left: 0;
+`;
+
+const Content = styled.div`
+	${maxPageContentWidthStyleObject}
+	position: relative;
+	h1 {
+		font-weight: 300;
+		line-height: 0.97;
+		text-wrap: balance; // supported in Chrome but not in safari
+		> span {
+			display: block;
+		}
+		+ * {
+			margin-top: 1.5em;
+		}
+	}
+	> * {
+		+ h1 {
+			margin-top: 3.2rem;
+		}
+	}
+	&.largeHeading {
+		h1 {
+		}
+		> * {
+			+ h1 {
+				margin-top: 4.8rem;
+			}
+		}
+	}
+	p {
+		margin-left: auto;
+		margin-right: auto;
+	}
+`;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -28,6 +28,7 @@ const inter = Inter({ subsets: ["latin"] });
 import { createGlobalStyle } from "styled-components";
 import { responsiveSizesCSSVariables } from "@/styles/responsiveSizes.styles";
 import { UploadFile } from "@/components/UploadFile";
+import HeroSection from "@/components/HeroSection";
 
 const GlobalStyle = createGlobalStyle`
   ${responsiveSizesCSSVariables}
@@ -120,49 +121,7 @@ export default function Home() {
 	);
 }
 
-const HeroSection = (
-	props: PropsWithChildren<{
-		largeHeading?: boolean;
-		backgroundColor?: string;
-		textColor?: string;
-		backdrop?: "grid" | "dots";
-		backdropColor?: string;
-		backdropPositionBottom?: string;
-		/** only available to the grid design */
-		backdropParallax?: boolean;
-		/** only available to the grid design */
-		backdropAnimateIn?: boolean;
-		backdropLayerChildren?: ReactNode;
-	}>
-) => {
-	const backdropColor = props.backdropColor ?? colors.purple100;
-	const hasBackground = props.backdrop || props.backdropColor;
 
-	return (
-		<HeroSectionWrap textColor={props.textColor}>
-			<Spacing size='xxl' />
-			{hasBackground && (
-				<HeroSectionBackdropLayer
-					id='HeroSectionBackdropLayer'
-					backgroundColor={props.backgroundColor}
-					offsetBottom={props.backdropPositionBottom}
-				>
-					<GridCanvas
-						color={backdropColor}
-						addBorderBottom
-						parallax={props.backdropParallax}
-						animateIn={props.backdropAnimateIn}
-					/>
-					{props.backdropLayerChildren}
-				</HeroSectionBackdropLayer>
-			)}
-			<Content className={cx(props.largeHeading && "largeHeading")}>
-				{props.children}
-			</Content>
-			{hasBackground && <Spacing size='sectionPadding' />}
-		</HeroSectionWrap>
-	);
-};
 
 const LooseDecorationsSetBottom = styled.div`
 	position: relative;
@@ -192,60 +151,11 @@ const DecoMiddleRight = styled.div`
 	bottom: 5em;
 `;
 
-const HeroSectionWrap = styled.header<{
-	textColor?: string;
-}>`
-	width: 100%;
-	position: relative;
-	text-align: center;
-	color: ${(p) => p.textColor ?? "inherit"};
-`;
 
-export const HeroSectionBackdropLayer = styled.div<{
-	backgroundColor?: string;
-	offsetBottom?: string;
-}>`
-	position: absolute;
-	top: 0;
-	right: 0;
-	background-color: ${(p) => p.backgroundColor ?? "transparent"};
-	bottom: ${(p) => p.offsetBottom || "0"};
-	left: 0;
-`;
 
-const Content = styled.div`
-	${maxPageContentWidthStyleObject}
-	position: relative;
-	h1 {
-		font-weight: 300;
-		line-height: 0.97;
-		text-wrap: balance; // supported in Chrome but not in safari
-		> span {
-			display: block;
-		}
-		+ * {
-			margin-top: 1.5em;
-		}
-	}
-	> * {
-		+ h1 {
-			margin-top: 3.2rem;
-		}
-	}
-	&.largeHeading {
-		h1 {
-		}
-		> * {
-			+ h1 {
-				margin-top: 4.8rem;
-			}
-		}
-	}
-	p {
-		margin-left: auto;
-		margin-right: auto;
-	}
-`;
+
+
+
 
 const Header = styled.div`
 	position: relative;
